3629c0d50cb69c229c39ffbb808e3fd4de914079
steve-chen-nyc <steve.l.chen@gmail.com>
2017-04-08 23:17:44 -0400

    configured webpack, linter, css, and first component

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname, // run in root directory
+  entry: './js/ClientApp.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js' // where bundle js to go
+  },
+  devServer: {
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        include: path.resolve(__dirname, 'js'),
+        test: /\.js$/, // if it passes this test then run loader
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
