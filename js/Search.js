884a878b6a3f13ba00d0e2ce9401dd0bb96da211
steve-chen-nyc <steve.l.chen@gmail.com>
2017-04-09 17:10:37 -0400

    added showcard component to search

--- js/Search.js
+++ js/Search.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+        {preload.shows.map((show) => {
+          return (
+            <ShowCard key={show.imdbID} show={show} />
+          )
+        })}
       </div>
     )
   }
