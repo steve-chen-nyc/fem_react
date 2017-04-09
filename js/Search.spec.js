6f2ee486cd4b92da3864628abce86d07c6a5f95f
steve-chen-nyc <steve.l.chen@gmail.com>
2017-04-09 18:45:38 -0400

    added a few more tests with enzyme and also added a script in package.json for coverage

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import Search from './Search'
-import renderer from 'react-test-renderer'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 
 test('Search snapshot test', () => {
-  const component = renderer.create(<Search />)
-  const tree = component.toJSON()
+  const component = shallow(<Search />) //one layer deep only
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(preload.shows.length).toEqual(component.find(
+    ShowCard).length)
+})
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value:
+    searchWord}})
+
+  const showCount = preload.shows.filter((show) => 
+              `${show.title} ${show.description}`.toUpperCase()
+              .indexOf(searchWord.toUpperCase()) >= 0).length
+
+  expect(component.find(ShowCard).length).toEqual(showCount)
 })
