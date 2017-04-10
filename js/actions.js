c5e353663c12e1617baac6494ea34d9cd6c2fdb2
steve-chen-nyc <steve.l.chen@gmail.com>
2017-04-10 00:24:31 -0400

    added thunk middleware to handle ajax request and async redux

--- js/actions.js
+++ js/actions.js
@@ -1,1 +1,2 @@
 export const SET_SEARCH_TERM = 'SET_SEARCH_TERM'
+export const ADD_OMBD_DATA = 'ADD_OMBD_DATA'
--- js/store.js
+++ js/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleWare } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose(
+  applyMiddleWare(thunk),
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 export default store
